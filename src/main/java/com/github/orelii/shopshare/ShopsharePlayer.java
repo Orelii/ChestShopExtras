47af33c3f4386d5f162e351160fa980986ab8654
Orelii <83521686+Orelii@users.noreply.github.com>
2025-03-27 15:34:44 -0600

    fixed error when trying to get claim outside of a claim

--- src/main/java/com/github/orelii/shopshare/ShopsharePlayer.java
+++ src/main/java/com/github/orelii/shopshare/ShopsharePlayer.java
@@ -3,41 +3,52 @@ package com.github.orelii.shopshare;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.DataStore;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ShopsharePlayer{
 
+    private static final MiniMessage miniMessage = MiniMessage.miniMessage();
+
     DataStore ds = GriefPrevention.instance.dataStore;
     Shopshare plugin = Shopshare.getPlugin(Shopshare.class);
-    String name = "";
-    String UUID = "";
-    Location location;
+    String _Name = "";
+    String _UUID = "";
+    Location _Location;
 
-    public ShopsharePlayer(String pname, String pUUID, Location plocation) {
-        name = pname;
-        UUID = pUUID;
-        location = plocation;
+    public ShopsharePlayer(String name, String uuid, Location location) {
+        _Name = name;
+        _UUID = uuid;
+        _Location = location;
+    }
+
+    public ShopsharePlayer(Player player) {
+        _Name = player.getName();
+        _UUID = player.getUniqueId().toString();
+        _Location = player.getLocation();
     }
 
     public void makeFile(){
-        File data = new File(plugin.getDataFolder(), File.separator + UUID + ".yml");
+        File data = new File(plugin.getDataFolder(), File.separator + _UUID + ".yml");
         FileConfiguration playerData = YamlConfiguration.loadConfiguration(data);
 
         if (!data.exists()) {
             try{
                 playerData.createSection("PlayerData");
-                playerData.set("PlayerData.name", name);
-                playerData.set("PlayerData.UUID", UUID);
+                playerData.set("PlayerData.name", _Name);
+                playerData.set("PlayerData.UUID", _UUID);
 
-                playerData.createSection("Trusted");
-                playerData.set("Trusted.list", new String[]{});
+                playerData.createSection("GlobalTrust");
+                playerData.set("GlobalTrust.list", new ArrayList<String>());
 
                 playerData.save(data);
 
@@ -48,8 +59,9 @@ public class ShopsharePlayer{
         }
     }
 
+
     public File getFile(){
-        File data = new File(plugin.getDataFolder(), File.separator + UUID + ".yml");
+        File data = new File(plugin.getDataFolder(), File.separator + _UUID + ".yml");
         if (!data.exists()) {
             return null;
         }
@@ -60,8 +72,11 @@ public class ShopsharePlayer{
     public List<String> getLocalTrustList(){
         File data = getFile();
         FileConfiguration playerData = YamlConfiguration.loadConfiguration(data);
+        Claim claim = getClaimAtLocation();
 
-        List<String> trusted = (List<String>) playerData.getList(getClaimAtLocation().getID().toString()+".list", null);
+        if (claim == null) { return null; }
+
+        List<String> trusted = (List<String>) playerData.getList(claim.getID().toString()+".list", null);
         if (trusted == null) { trusted = new ArrayList<String>(); }
 
         return trusted;
@@ -69,6 +84,18 @@ public class ShopsharePlayer{
 
 
     public Claim getClaimAtLocation(){
-        return ds.getClaimAt(location, false, ds.getPlayerData(java.util.UUID.fromString(UUID)).lastClaim);
+        return ds.getClaimAt(_Location, false, ds.getPlayerData(java.util.UUID.fromString(_UUID)).lastClaim);
+    }
+
+    public String getName(){
+        return _Name;
+    }
+
+    public UUID getUUID(){
+        return UUID.fromString(_UUID);
+    }
+
+    public Location getLocation() {
+        return _Location;
     }
 }
